053f98aeda95f4171109a274974430ce0f9ba93d
Janavi Reddy <Janavi Reddy@DESKTOP-73OQVUV>
2022-02-04 10:40:53 +0530

    first commit

---
+++ 04-02-2022/src/com/javaDatatype/example1.java
@@ -0,0 +1,14 @@
+package com.javaDatatype;
+
+public class example1 {
+
+	public static void main(String[] args) {
+
+     //java program to print hello janavi
+	 System.out.println("hello");
+     System.out.println("janavi");
+  
+     
+	}
+
+}
